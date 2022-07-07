36275df341f4c64bf7e8433adffb4f7c7d5fd9a4
somang-lim <somangjuha@gmail.com>
2022-07-07 16:02:03 +0900

    WiseSaying 클래스 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -23,6 +23,10 @@ public class App {
                     System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
                     int id = ++wiseSayingLastId; // 명언 글 번호 증가
+
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    System.out.println(wiseSaying);
+
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
 
