1a392ae1a029fab6c50ccb781d19c624bd7220a2
somang-lim <somangjuha@gmail.com>
2022-07-07 15:46:07 +0900

    글 등록할 때마다 글 번호 증가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -9,12 +9,23 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
+        int wiseSayingLastId = 0;
+
         outer:
         while(true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
 
             switch(cmd) {
+                case "등록" :
+                    System.out.printf("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.printf("작가 : ");
+                    String author = sc.nextLine().trim();
+                    int id = ++wiseSayingLastId; // 명언 글 번호 증가
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
+
                 case "종료" :
                     break outer;
             }
