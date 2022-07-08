eba58898299221040b0f56acaaca4ef8bfd6705c
somang-lim <somangjuha@gmail.com>
2022-07-08 14:44:36 +0900

    입력받은 명언을 리스트에 저장, 목록 기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,5 +1,7 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -9,6 +11,7 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         int wiseSayingLastId = 0;
 
         outer:
@@ -25,11 +28,18 @@ public class App {
                     int id = ++wiseSayingLastId; // 명언 글 번호 증가
 
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
-                    System.out.println(wiseSaying);
+                    wiseSayings.add(wiseSaying);
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
-
+                case "목록" :
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("---------------");
+                    for(int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
+                    break;
                 case "종료" :
                     break outer;
             }
--- src/main/java/com/ll/exam/WiseSaying.java
+++ src/main/java/com/ll/exam/WiseSaying.java
@@ -2,9 +2,9 @@ package com.ll.exam;
 
 public class WiseSaying {
 
-    private int id;
-    private String content;
-    private String author;
+    int id;
+    String content;
+    String author;
 
     public WiseSaying(int id, String content, String author) {
         this.id = id;
