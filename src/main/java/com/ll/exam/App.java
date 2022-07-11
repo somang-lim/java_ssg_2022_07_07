776e9570d41213ef68aa0b374562380573317b32
somang-lim <somangjuha@gmail.com>
2022-07-11 15:27:21 +0900

    findById 메서드 도입

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -79,26 +79,27 @@ public class App {
         }
 
         // URL에 입력된 id에 해당하는 명언객체 찾기
-        WiseSaying wiseSaying__ = null;
-
-        for(WiseSaying wiseSaying___ : wiseSayings) {
-            if(wiseSaying___.id == paramId) {
-                wiseSaying__ = wiseSaying___;
-            }
-        }
+        WiseSaying foundWiseSaying = findById(paramId);
 
         // 찾지 못했다면 중지
-        if(wiseSaying__ == null) {
+        if(foundWiseSaying == null) {
             System.out.printf("%d번 명언은 존재하지 않습니다.\n", paramId);
             return;
         }
 
         // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
-        wiseSayings.remove(wiseSaying__);
+        wiseSayings.remove(foundWiseSaying);
 
         System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
     }
 
-
+    private WiseSaying findById(int paramId) {
+        for(WiseSaying wiseSaying : wiseSayings) {
+            if(wiseSaying.id == paramId) {
+                return wiseSaying;
+            }
+        }
+        return null;
+    }
 
 }
